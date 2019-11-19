bd38bea0254340648cf4e849033303bbe6e7d26f
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-19 12:59:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -44,7 +44,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.where(title: 'Title', release_date: 2000, director: 'Me')
+  movie = Movie.where(title: 'Title', release_date: 2000, director: 'Me')
   # Movie.all.detect {|movie| movie.title == "title" && movie.release_date == 2000 && movie.director == "Me"}
 
 end
