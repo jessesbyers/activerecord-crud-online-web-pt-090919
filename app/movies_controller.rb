6d01cce457a4d1ef3425997ba79ee45de3a5fa73
Jesse Byers <jesse.s.byers@gmail.com>
2019-11-19 13:06:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,16 +38,8 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.find(1)
 end
 
-# not passing
 def can_find_by_multiple_attributes
-  # Search Values:
-  # title == "Title"
-  # release_date == 2000
-  # director == "Me"
   Movie.find_by(title: 'Title', release_date: 2000, director: 'Me')
-  # movie
-  # Movie.all.detect {|movie| movie.title == "title" && movie.release_date == 2000 && movie.director == "Me"}
-
 end
 
 def can_find_using_where_clause_and_be_sorted
@@ -58,7 +50,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick").save
-  # movie.save
+  movie.save
 end
 
 def can_update_using_update_method
